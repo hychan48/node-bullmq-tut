5cf5684bb4ebacb737b53dcef9c304a3f2aa0be7
Chan, Ho Yan <HoYan.Chan@amd.com>
2023-04-20 11:15:32 -0400

    got it to work... that's what i thought

--- experiments/launch_firefox/launchFirefox.nt.mjs
+++ experiments/launch_firefox/launchFirefox.nt.mjs
@@ -2,7 +2,10 @@ import {execCmdOnController} from "./SpawnExecOnController.mjs";
 
 /* cmd commands */
 export const killFirefoxCmd = "taskkill /IM firefox.exe /F"
-export const launchFirefoxCmd = `"C:\\Program Files\\Mozilla Firefox\\firefox.exe"`
+// export const killFirefoxCmd = "taskkill /IM notepad.exe /F"
+// export const launchFirefoxCmd = `"C:\\Program Files\\Mozilla Firefox\\firefox.exe" --browser`
+export const launchFirefoxCmd = `call "C:\\Program Files\\Mozilla Firefox\\firefox.exe"`
+// export const launchFirefoxCmd = `start notepad` //problem is it hangs
 export async function launchFirefoxNT(taskKill=false){
   let out
   if(taskKill){
---
+++ experiments/launch_firefox/launchListener.mjs
@@ -0,0 +1,31 @@
+/**
+ * https://docs.bullmq.io/readme-1
+ *
+ * https://www.reddit.com/r/googlecloud/comments/10k4v2t/why_suddenly_much_higher_costs_in_google_cloud/
+ */
+import { Worker } from 'bullmq';
+import {queueName} from "../../bullmq/bullMQConstants.mjs";
+import {launchFirefoxNT} from "./launchFirefox.nt.mjs";
+// const queueName = 'myJobName'
+
+const worker = new Worker(queueName, async job => {
+  // Will print { foo: 'bar'} for the first job
+  // and { qux: 'baz' } for the second.
+  console.log('job.data',job.data);
+  await launchFirefoxNT(false)
+
+},{
+  connection:{
+    host: "localhost",
+    port: 6379,
+  }
+  }
+);
+worker.on('completed', job => {
+  console.log(`${job.id} has completed!`);
+});
+
+worker.on('failed', (job, err) => {
+  console.log(`${job.id} has failed with ${err.message}`);
+});
+console.log('started');
